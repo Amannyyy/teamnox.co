7a22f90c4c4f8b7bb1b178137de8481ef6a6b9f3
Aman Ehan <aehan2009@gmail.com>
2026-08-22 04:22:06 -0400

    Connect live Calendar test

--- Resources/calendar.js
+++ Resources/calendar.js
@@ -1252,35 +1252,3 @@ filters.forEach(
 ========================================================= */
 
 loadCalendar();
-
-export async function onRequest() {
-  const calendarUrl =
-    "https://calendar.google.com/calendar/ical/8f283dbd87e4b9a262ab60329952855b83c6e316a19bb792905e612532740939%40group.calendar.google.com/public/basic.ics";
-
-  try {
-    const response = await fetch(calendarUrl);
-
-    if (!response.ok) {
-      return new Response(
-        "Unable to load Google Calendar.",
-        { status: 500 }
-      );
-    }
-
-    const ics = await response.text();
-
-    return new Response(ics, {
-      status: 200,
-      headers: {
-        "Content-Type": "text/calendar; charset=utf-8",
-        "Cache-Control": "public, max-age=300"
-      }
-    });
-
-  } catch (error) {
-    return new Response(
-      "Calendar request failed.",
-      { status: 500 }
-    );
-  }
-}
